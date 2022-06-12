46fb2a7a31a5826dd88d34bb3e1fee2e4e57d1cd
mkpbd <mostofakamal1990@gmail.com>
2022-06-12 14:09:48 +0600

    Route not found Error message

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Container, Row,Col } from 'react-bootstrap';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Something was Wrong</h2>
-        </div>
+       <Container>
+           <Row>
+               <Col className='offset-sm-3 my-4'>
+                <img className='img-fluid' src="/images/notfound.jpg" alt="" />
+               </Col>
+           </Row>
+       </Container>
     );
 };
 
